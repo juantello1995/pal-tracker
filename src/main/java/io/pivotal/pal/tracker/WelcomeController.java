14d80581a39f7c379120e95878aa1e3f5b166cd1
juantello1995 <ubuntu@ip-172-31-32-60.ec2.internal>
2021-06-15 16:21:56 +0000

    Yes

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,26 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+@RestController
+public class WelcomeController {
+
+    private String message;
+
+    public WelcomeController( @Value("${welcome.message}") String messages) {
+        this.message = messages;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+
+
+        String messages = this.message;
+        return messages;
+    }
 }
